15c2457c53c1584c656cc288d1ca018f0e60a20e
Geoffrey Kleinberg <gkleinberg21@gmail.com>
2023-04-30 18:18:10 -0400

    final adjustments, added comments

--- cubefield.js
+++ cubefield.js
@@ -1,19 +1,15 @@
 // Cubefield Game
 // Based on https://www.cubefield.org.uk/
+// Authors: Braden Kirkpatrick, Geoffrey Kleinberg
 'use strict';
 
-
-// TODO:
-// improvements?
 
 // Global WebGL context variable
 let gl;
 
 // Allow use of glMatrix values directly instead of needing the glMatrix prefix
 const vec3 = glMatrix.vec3;
-const vec4 = glMatrix.vec4;
 const mat4 = glMatrix.mat4;
-const quat = glMatrix.quat;
 
 
 // stores all the objects we have
@@ -21,21 +17,25 @@ let plane;
 let cubes = [];
 let tetra;
 
-let planeScale = 100;
-let tetraScale = 0.03;
-let cubeScale = 0.04;
+// defines the scale of the objects
+const PLANE_SCALE = 100;
+const TETRA_SCALE = 0.03;
+const CUBE_SCALE = 0.04;
 
 // allocate matrices globally
 let projectionMatrix = mat4.create();
 
+// keeps track of movement of environment
 let envDx = 0;
 let speed = -1 / 100;
 const MAX_SPEED = -1 / 10;
 
+// defines the view matrix
 let eye = vec3.fromValues(0, 0.25, -0.75);
 let horizon = vec3.fromValues(0, 0.25, 100);
 let up = vec3.fromValues(0, 1, 2);
 
+// keeps track of rotation
 let upRotation = 0;
 let dUp = 0;
 let maxRight = 0.1;
@@ -91,7 +91,10 @@ window.addEventListener('load', function init() {
     // set to size of window
     onWindowResize();
 
+    // gets the initial difficulty
     difficulty = document.getElementById('difficulty').value;
+
+    // sets the default view matrix
     let viewMatrix = mat4.lookAt(mat4.create(), eye, horizon, up);
     gl.uniformMatrix4fv(gl.program.uViewMatrix, false, viewMatrix);
 
@@ -184,6 +187,7 @@ function initProgram() {
                 specular = pow(max(dot(R, E), 0.0), materialShininess);
             }
             
+            // texture is only used for objects with texture
             vec4 color;
             if (uTextured) {
                 color = texture(uTexture, vTextureCoord);
@@ -195,6 +199,7 @@ function initProgram() {
             fragColor.rgb =
                 ((materialAmbient + materialDiffuse * diffuse) * color.rgb
                 + materialSpecular * specular) * lightColor;
+            // brickwall.png has translucency, so we use original alpha
             fragColor.a = vColor.a;
         }`
     );
@@ -219,6 +224,9 @@ function initProgram() {
 }
 
 
+/**
+ * Initializs the buffers for all objects
+ */
 function initBuffers() {
     // create buffers for cube/tetrahedron data
     // create buffer for horizon
@@ -252,8 +260,10 @@ function initBuffers() {
         0, 0, 0, 1,
         0, 0, 0, 1
     ];
+    // eslint-disable-next-line no-undef
     let cubeNormals = calc_normals(Float32Array.from(cubeCoords), cubeIndices, false);
 
+    // eslint-disable-next-line no-undef
     gl.cubeVao = createVao(gl, [
         [gl.program.aPosition, cubeCoords, 3],
         [gl.program.aColor, cubeColors, 4],
@@ -261,13 +271,15 @@ function initBuffers() {
     ], cubeIndices);
 
     // The vertices, colors, and indices for a tetrahedron
+    // elevated slightly off the ground (to avoid translucency issues)
     let tetraCoords = [
-        0, 4/3, Math.sqrt(2/9),
-        0, 0, Math.sqrt(2),
-        Math.sqrt(2/3), 0, 0,
-        -Math.sqrt(2/3), 0, 0
+        0, 4/3, Math.sqrt(2/9), // top
+        0, 0.005, Math.sqrt(2), // front
+        Math.sqrt(2/3), 0.005, 0, // back right
+        -Math.sqrt(2/3), 0.005, 0 // back left
     ];
-    let tetraIndices = [1, 3, 0, 2, 1, 3];
+    let tetraIndices = [2, 1, 3, 0, 2, 1];
+    tetraIndices = [3, 2, 1, 0, 3, 2];
     let tetraAlpha = 0.5;
     let tetraColors = [
         1, 0, 0, tetraAlpha,// green
@@ -276,14 +288,17 @@ function initBuffers() {
         1, 0, 0, tetraAlpha
     ];
 
+    // eslint-disable-next-line no-undef
     let tetraNormals = calc_normals(Float32Array.from(tetraCoords), tetraIndices, true);
 
+    // eslint-disable-next-line no-undef
     gl.tetraVao = createVao(gl, [
         [gl.program.aPosition, tetraCoords, 3], 
         [gl.program.aColor, tetraColors, 4],
         [gl.program.aNormal, tetraNormals, 3]
     ], tetraIndices);
 
+    // The vertices, colors, and indices for a plane
     let planeCoords = [
         1, 0, 1,
         -1, 0, 1,
@@ -297,8 +312,10 @@ function initBuffers() {
         0.4, 1, 0.4, 1,
         0.4, 1, 0.4, 1
     ];
+    // eslint-disable-next-line no-undef
     let planeNormals = calc_normals(Float32Array.from(planeCoords), planeIndices, true);
 
+    // eslint-disable-next-line no-undef
     gl.planeVao = createVao(gl, [
         [gl.program.aPosition, planeCoords, 3], 
         [gl.program.aColor, planeColors, 4],
@@ -307,11 +324,16 @@ function initBuffers() {
 
 }
 
+/**
+ * Initializes the brick wall texture
+ */
 function initTextures() {
 
     let image = new Image();
     image.src = 'brickwall.png';
     image.addEventListener('load', () => {
+        // load it as a cubemap so it appears on all sides of the cube
+        // eslint-disable-next-line no-undef
         gl.cubeTexture = loadCubemapTexture(gl, image, image, image, image, image, image, 0);
         render();
 
@@ -319,26 +341,37 @@ function initTextures() {
 
 }
 
+/**
+ * Generates a new cube at the specified (x, z) location
+ */
 function generateObject(x, z) {
-    let mv = mat4.scale(mat4.create(), mat4.create(), [cubeScale, cubeScale, cubeScale]);
+    let mv = mat4.scale(mat4.create(), mat4.create(), [CUBE_SCALE, CUBE_SCALE, CUBE_SCALE]);
     mat4.translate(mv, mv, [x, 1, z]);
 
-    // need to draw these first for translucency, so add them at the beginning of array
     cubes.push([gl.cubeVao, gl.TRIANGLES, 36, gl.cubeTexture, mv, .1, speed, (Math.random() - 0.5) * difficultyMultiplier]);
-
 }
+
+/**
+ * Generates a row of cubes
+ */
 function generateNewCubes() {
     for (let i = 0; i < 100; i++) {
+        // aligns the x to a grid
         let x = i * 2 - 99;
+        // 7% chance of generating a cube at each location
         if (Math.random() < 0.07) {
+            // make it farther away based on the speed we are traveling
             generateObject(x, -10000 * speed)
         }
     }
 }
 
+/**
+ * Initializes the global variables for the plane and tetrahedron
+ */
 function initObjects() {
-    plane = [gl.planeVao, gl.TRIANGLE_STRIP, 4, null, mat4.scale(mat4.create(), mat4.create(), [planeScale, planeScale, planeScale]), 1, 0, 0];
-    tetra = [gl.tetraVao, gl.TRIANGLE_STRIP, 6, null, mat4.scale(mat4.create(), mat4.create(), [tetraScale, tetraScale, tetraScale]), 1, 0, 0];
+    plane = [gl.planeVao, gl.TRIANGLE_STRIP, 4, null, mat4.scale(mat4.create(), mat4.create(), [PLANE_SCALE, PLANE_SCALE, PLANE_SCALE]), 1, 0, 0];
+    tetra = [gl.tetraVao, gl.TRIANGLE_STRIP, 6, null, mat4.scale(mat4.create(), mat4.create(), [TETRA_SCALE, TETRA_SCALE, TETRA_SCALE]), 1, 0, 0];
 }
 
 /**
@@ -347,16 +380,16 @@ function initObjects() {
 function initEvents() {
 
     //add listeners
-
     window.addEventListener('resize', onWindowResize);
     window.addEventListener('keydown', onKeyDown);
-
     document.getElementById('restart').addEventListener('click', restart);
-
     document.getElementById('difficulty').addEventListener('input', changeDifficulty);
 
 }
 
+/**
+ * Restarts the game (score, cubes, speed, etc)
+ */
 function restart() {
     score = 0;
     cubes = [];
@@ -370,13 +403,21 @@ function restart() {
     setDifficulty();
 }
 
+/**
+ * Sets the difficulty and restarts when the HTML is interacted with
+ */
 function changeDifficulty() {
     difficulty = document.getElementById('difficulty').value;
     setDifficulty();
     restart();
 }
 
+/**
+ * Sets difficultyMultiplier and dScore based on difficulty
+ */
 function setDifficulty() {
+    // difficultyMultiplier gets multiplied by the random to determine x velocity
+    // dScore increases on harder difficulty so we get points faster
     if (difficulty === "Easy") {
         difficultyMultiplier = 0;
         dScore = 1;
@@ -395,6 +436,8 @@ function setDifficulty() {
     }
 }
 
+
+// animation variables
 let last_redraw;
 let last_object = 0;
 let elapsed;
@@ -414,6 +457,7 @@ function render(ms) {
         elapsed = 0;
     }
     
+    // generates new objects after a certain delay based on speed
     last_object += elapsed
     let delay = -2.5 / speed
     if(last_object > delay) {
@@ -421,23 +465,31 @@ function render(ms) {
         generateNewCubes();
     } 
 
+    // if we are rotating, then rotate
     if (dUp > 0) {
         upRotation = Math.min(maxRight, upRotation + dUp);
     } else if (dUp < 0) {
         upRotation = Math.max(minLeft, upRotation + dUp);
     }    
-
     updateViewMatrix(upRotation);
 
+    // order of objects is based on translucency
+
+    // draw the plane first
     drawObject(plane);
 
+    // draw each of the cubes
     for (let cube of cubes) {
+        // first translate the mv matrix
         let [mv, scale, dz, dx] = cube.slice(4);
         mat4.translate(mv, mv, [(dx + envDx) * elapsed / scale, 0, dz * elapsed / scale]);
         // check if mv makes the cube collide with tetrahedron
         checkCollision(mv);
+        // then draw it
         drawObject(cube);
     }
+
+    // draw the tetrahedron
     drawObject(tetra);
 
     //remove objects that are outside of camera
@@ -450,6 +502,7 @@ function render(ms) {
         
     }
     
+    // increase the score, update high score
     if (playing) {
         score += dScore;
         runTime += 1;
@@ -457,46 +510,70 @@ function render(ms) {
     document.getElementById('score').innerHTML = "Score: " + Math.round(score);
     highScores[difficulty] = Math.max(score, highScores[difficulty]);
     document.getElementById('hs' + difficulty).innerHTML = "High Score (" + difficulty +"): " + Math.round(highScores[difficulty]);
+
+    // increase the speed and dScore as we survive longer
     if (runTime % 100 === 0) {
         dScore *= 1.025;
         // max becuase they're negative
         speed = Math.max(speed * 1.01, MAX_SPEED);
     }
 
+    // re-render the scene
     window.requestAnimationFrame(render);
 
 
 }
 
+
+/**
+ * Draws a single object
+ */
 function drawObject(obj) {
     let [vao, type, count, texture, mv] = obj;
+    // bind VAO
     gl.bindVertexArray(vao);
+    // select if it should be textured or not
     if (texture) {
         gl.bindTexture(gl.TEXTURE_CUBE_MAP, texture);
         gl.uniform1i(gl.program.uTextured, 1);
     } else {
         gl.uniform1i(gl.program.uTextured, 0);
     }
+    // send mv matrix to uniform
     gl.uniformMatrix4fv(gl.program.uModelViewMatrix, false, mv);
+    // draw
     gl.drawElements(type, count, gl.UNSIGNED_SHORT, 0);
+    // cleanup
     gl.bindTexture(gl.TEXTURE_2D, null);
     gl.bindVertexArray(null);
 }
 
+
+/**
+ * Checks if a cube collides with the tetrahedron based on mv
+ */
 function checkCollision(mv) {
     // helps with collision detection at high speeds
-    let collisionOffset = -1 * speed * 0.05;
+    let collisionOffset = -1 * speed * 0.04;
+
+    // center (x, z) of the cube
     let centerX = mv[12];
     let centerZ = mv[14];
 
-    let tetraPointZ = Math.sqrt(2) * tetraScale + collisionOffset;
-    let tetraRightX = Math.sqrt(2/3) * tetraScale + collisionOffset;
-    let tetraLeftX = -Math.sqrt(2/3) * tetraScale - collisionOffset;
+    // the three ground vertices of the tetrahedron
+    let tetraPointZ = Math.sqrt(2) * TETRA_SCALE + collisionOffset;
+    let tetraRightX = Math.sqrt(2/3) * TETRA_SCALE + collisionOffset;
+    let tetraLeftX = -Math.sqrt(2/3) * TETRA_SCALE - collisionOffset;
 
-    let baseZ = centerZ - cubeScale - collisionOffset;
-    let backZ = centerZ + cubeScale + collisionOffset;
-    let leftX = centerX - cubeScale - collisionOffset;
-    let rightX = centerX + cubeScale + collisionOffset;
+    // the 4 ground vertices of the cube
+    let baseZ = centerZ - CUBE_SCALE - collisionOffset;
+    let backZ = centerZ + CUBE_SCALE + collisionOffset;
+    let leftX = centerX - CUBE_SCALE - collisionOffset;
+    let rightX = centerX + CUBE_SCALE + collisionOffset;
+
+    // x0, x1, z0, and z1 are calculated from intersection of lines
+    // if the (x, z) value is on both lines, then it intersects
+
     // check if front line of cube intersects tetrahedron
     let x0 = (baseZ - tetraPointZ) / Math.sqrt(3);
     if (x0 >= tetraLeftX && x0 <= collisionOffset && x0 >= leftX && x0 <= rightX) {
@@ -508,21 +585,21 @@ function checkCollision(mv) {
     }
 
     // check if left line of cube intersects tetrahedron
-    let z0 = leftX * Math.sqrt(3) + tetraScale * Math.sqrt(2);
+    let z0 = leftX * Math.sqrt(3) + TETRA_SCALE * Math.sqrt(2);
     if (z0 >= -1 * collisionOffset && z0 <= tetraPointZ && z0 >= baseZ && z0 <= backZ) {
         playing = false;
     }
-    let z1 = leftX * -Math.sqrt(3) + tetraScale * Math.sqrt(2);
+    let z1 = leftX * -Math.sqrt(3) + TETRA_SCALE * Math.sqrt(2);
     if (z1 >= -1 * collisionOffset && z1 <= tetraPointZ && z1 >= baseZ && z1 <= backZ) {
         playing = false;
     }
 
     // check if right line of cube intersects tetrahedron
-    z0 = rightX * Math.sqrt(3) + tetraScale * Math.sqrt(2);
+    z0 = rightX * Math.sqrt(3) + TETRA_SCALE * Math.sqrt(2);
     if (z0 >= -1 * collisionOffset && z0 <= tetraPointZ && z0 >= baseZ && z0 <= backZ) {
         playing = false;
     }
-    z1 = rightX * -Math.sqrt(3) + tetraScale * Math.sqrt(2);
+    z1 = rightX * -Math.sqrt(3) + TETRA_SCALE * Math.sqrt(2);
     if (z1 >= -1 * collisionOffset && z1 <= tetraPointZ && z1 >= baseZ && z1 <= backZ) {
         playing = false;
     }
@@ -540,6 +617,9 @@ function onWindowResize() {
     updateProjectionMatrix();
 }
 
+/**
+ * Creates and updates the projection matrix
+ */
 function updateProjectionMatrix() {
     // Create the perspective projection matrix
     let [w, h] = [gl.canvas.width, gl.canvas.height];
@@ -553,55 +633,80 @@ function updateProjectionMatrix() {
     gl.uniformMatrix4fv(gl.program.uProjectionMatrix, false, projectionMatrix);
 }
 
+/**
+ * Changes the view matrix based on the up angle
+ */
 function updateViewMatrix(angle) {
 
+    // calculates the rotation matrix for the angle rotating around z-axis
     let rotation = mat4.fromRotation(mat4.create(), angle, [0, 0, 1]);
 
+    // calculates the new up vector
     let thisUp = vec3.transformMat4(vec3.create(), up, rotation)
 
+    // calculates and updates the new view matrix
     let viewMatrix = mat4.lookAt(mat4.create(), eye, horizon, thisUp);
     gl.uniformMatrix4fv(gl.program.uViewMatrix, false, viewMatrix);
 }
 
+/**
+ * Fires whenever a key is pressed, controls motion
+ */
 function onKeyDown(e) {
+    // add listener for when the key is raised
     window.addEventListener('keyup', onKeyUp);
 
+    // if we are playing and press right
     if (e.keyCode === 37 && playing) {
         e.preventDefault();
         // move right
         envDx = -1 / 150;
+        // angle environment to right
         maxRight = 0.1;
         minLeft = 0;
         dUp = 0.01
+    // if we are playing and press left
     } else if (e.keyCode === 39 && playing) {
         e.preventDefault();
         // move left
         envDx = 1 / 150;
+        // angle environment to left
         minLeft = -0.1;
         maxRight = 0;
         dUp = -0.01;
+    // if we press r
     } else if (e.keyCode === 82) {
         restart();
     }
 
 }
 
+/**
+ * Fires whenever a key is raised
+ */
 function onKeyUp(e) {
 
+    // if we release right
     if (e.keyCode === 37 && playing) {
         e.preventDefault();
+        // stop moving
         envDx = 0;
+        // return angle to straight up
         maxRight = 0;
         minLeft = 0;
         dUp = -0.01;
+    // if we release left
     } else if (e.keyCode === 39 && playing) {
         e.preventDefault();
+        // stop moving
         envDx = 0;
+        // return angle to straight up
         minLeft = 0;
         maxRight = 0;
         dUp = 0.01;
     }
 
+    // don't listen for keyUp anymore
     window.removeEventListener('keyUp', onKeyUp);
 
 }
